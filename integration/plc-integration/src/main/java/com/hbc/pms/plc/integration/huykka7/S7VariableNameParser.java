132f00bc79316590be91efebbf68dc3c4c5fee20
Kirin Tran <tran123456k@gmail.com>
2023-12-24 01:50:08 +0700

    improve based on SonaCloud suggestions

--- integration/plc-integration/src/main/java/com/hbc/pms/plc/integration/huykka7/S7VariableNameParser.java
+++ integration/plc-integration/src/main/java/com/hbc/pms/plc/integration/huykka7/S7VariableNameParser.java
@@ -7,30 +7,30 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class S7VariableNameParser {
+
+  private final Map<String, DataType> types = new HashMap<>();
+
+  public S7VariableNameParser() {
+    types.put("x", DataType.BIT);
+    types.put("dbx", DataType.BIT);
+    types.put("s", DataType.STRING);
+    types.put("string", DataType.STRING);
+    types.put("b", DataType.BYTE);
+    types.put("dbb", DataType.BYTE);
+    types.put("d", DataType.DOUBLE);
+    types.put("int", DataType.INTEGER);
+    types.put("dint", DataType.D_INTEGER);
+    types.put("w", DataType.INTEGER);
+    types.put("dbw", DataType.INTEGER);
+    types.put("dul", DataType.U_LONG);
+    types.put("dulint", DataType.U_LONG);
+    types.put("dulong", DataType.U_LONG);
+  }
+
   private final Pattern regex =
       Pattern.compile(
-          "^(?<operand>db{1})(?<dbNr>\\d{1,4})\\.?(?<type>dbx|x|s|string|b|dbb|d|int|dbw|w|dint|dul|dulint|dulong|){1}(?<start>\\d+)(\\.(?<bitOrLength>\\d+))?$",
+          "^(?<operand>db)(?<dbNr>\\d{1,4})\\.(?<type>dbx|x|s|string|b|dbb|d|int|dbw|w|dint|dul|dulint|dulong|)(?<start>\\d+)(\\.(?<bitOrLength>\\d+))?$",
           Pattern.CASE_INSENSITIVE);
-
-  private final Map<String, DataType> types =
-      new HashMap<>() {
-        {
-          put("x", DataType.BIT);
-          put("dbx", DataType.BIT);
-          put("s", DataType.STRING);
-          put("string", DataType.STRING);
-          put("b", DataType.BYTE);
-          put("dbb", DataType.BYTE);
-          put("d", DataType.DOUBLE);
-          put("int", DataType.INTEGER);
-          put("dint", DataType.D_INTEGER);
-          put("w", DataType.INTEGER);
-          put("dbw", DataType.INTEGER);
-          put("dul", DataType.U_LONG);
-          put("dulint", DataType.U_LONG);
-          put("dulong", DataType.U_LONG);
-        }
-      };
 
   public S7VariableAddress parse(String input) {
     Matcher match = regex.matcher(input);
