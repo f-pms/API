f6cd5996ffc7ba475d2b55e81acd7da195aadac2
Nguyễn Nhật Huy <nguyennhathuy.orit@gmail.com>
2024-01-04 11:06:49 +0700

    test(PMS-158): Setup testcontainer + test framework (#9)

---
+++ core/core-api/src/test/java/com/hbc/pms/core/api/PLCTest.java
@@ -0,0 +1,38 @@
+package com.hbc.pms.core.api;
+
+import com.hbc.pms.plc.integration.mokka7.S7Client;
+import com.hbc.pms.plc.integration.mokka7.exception.S7Exception;
+import com.hbc.pms.plc.integration.mokka7.type.AreaType;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.startupcheck.IndefiniteWaitOneShotStartupCheckStrategy;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+public class PLCTest {
+
+  @Container
+  private final GenericContainer<?> server = new GenericContainer<>(
+        DockerImageName.parse("huybui479/plc_sim:latest_linux"))
+      .withExposedPorts(102, 80)
+      .withCopyFileToContainer(
+          MountableFile.forClasspathResource("datablocks.json"),
+          "/data/datablocks.json"
+      );
+
+  @Test
+  public void test() throws IOException, InterruptedException, S7Exception {
+    var client = new S7Client();
+    client.connect(server.getHost(), server.getFirstMappedPort(), 0, 1);
+    var result = client.readString(AreaType.DB, 1, 0, 10);
+    assertEquals("Hello, S7!", result);
+  }
+}
