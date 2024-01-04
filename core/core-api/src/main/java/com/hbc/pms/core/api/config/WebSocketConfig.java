0a8322b00737a28167d197277087f8bd9788a24a
Thinh Le <letienthinh0109@gmail.com>
2024-01-04 14:14:06 +0700

    removed hard code

--- core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
@@ -1,19 +1,23 @@
 package com.hbc.pms.core.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    public static final String LOCAL_ORIGIN = "http://localhost:5173";
+    @Value("#{'${hbc.origins}'.split(',')}")
+    private List<String> allowedOrigins;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins(LOCAL_ORIGIN);
+        registry.addEndpoint("/websocket").setAllowedOrigins(allowedOrigins.toArray(String[]::new));
     }
 
     @Override
