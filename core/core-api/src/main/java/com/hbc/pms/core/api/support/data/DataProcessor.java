a71e047ee685ec53b6e5e6dc347e71da01dcfb07
Le Tien Thinh <59760568+thinhle19@users.noreply.github.com>
2024-01-05 22:31:52 +0700

    fix(PMS-43): Change format of returned PLC data to front end (#13)

--- core/core-api/src/main/java/com/hbc/pms/core/api/support/data/DataProcessor.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/support/data/DataProcessor.java
@@ -2,6 +2,7 @@ package com.hbc.pms.core.api.support.data;
 
 import com.hbc.pms.plc.integration.huykka7.IoResponse;
 import com.hbc.pms.plc.integration.mokka7.exception.S7Exception;
+import com.hbc.pms.plc.io.Blueprint;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -14,6 +15,20 @@ public class DataProcessor {
 
         for (Map.Entry<String, IoResponse> entry : rawData.entrySet()) {
             flattenedData.put(entry.getKey(), entry.getValue().getValue().toString());
+        }
+        return flattenedData;
+    }
+
+    public Map<String, String> flattenToFigureMappedData(Map<String, IoResponse> rawData, Blueprint blueprint) throws S7Exception {
+        Map<String, String> flattenedData = new HashMap<>();
+        var addressToFigureMap = blueprint.getAddressToFiguresMap();
+
+        for (Map.Entry<String, IoResponse> entry : rawData.entrySet()) {
+            var valuesByAddress = addressToFigureMap.get(entry.getKey());
+
+            for (String val : valuesByAddress) {
+                flattenedData.put(val, entry.getValue().getValue().toString());
+            }
         }
         return flattenedData;
     }
