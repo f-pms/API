b29041964a52af8af045b07141dd2bb9f09fad20
oHTGo <nguyennhathuy.orit@gmail.com>
2024-01-12 12:31:56 +0700

    Remove BlueprintManager and update AppScheduler to use BlueprintService

--- core/core-api/src/main/java/com/hbc/pms/core/api/task/AppScheduler.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/task/AppScheduler.java
@@ -1,40 +1,31 @@
 package com.hbc.pms.core.api.task;
 
-import com.hbc.pms.core.api.service.BlueprintManager;
+import com.hbc.pms.core.api.service.BlueprintService;
 import com.hbc.pms.core.api.support.data.DataFetcher;
 import com.hbc.pms.core.api.support.data.DataProcessor;
 import com.hbc.pms.core.api.support.data.WebSocketPublisher;
 import com.hbc.pms.plc.integration.mokka7.exception.S7Exception;
 import com.hbc.pms.plc.io.Blueprint;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class AppScheduler {
-    private final BlueprintManager blueprintManager;
+    private final BlueprintService blueprintService;
     private final DataFetcher dataFetcher;
     private final DataProcessor dataProcessor;
     private final WebSocketPublisher webSocketPublisher;
-
-    public AppScheduler(
-        BlueprintManager blueprintManager,
-        DataFetcher dataFetcher,
-        DataProcessor dataProcessor,
-        WebSocketPublisher webSocketPublisher
-    ) {
-        this.blueprintManager = blueprintManager;
-        this.dataFetcher = dataFetcher;
-        this.dataProcessor = dataProcessor;
-        this.webSocketPublisher = webSocketPublisher;
-    }
 
     @Scheduled(fixedRate = 2000)
     public void refreshAllStationsState() {
         try {
             long startTime = System.currentTimeMillis();
-            for (Blueprint blueprint : blueprintManager.getBlueprints()) {
+            // TODO: add cache for blueprintService.getAll()
+            for (Blueprint blueprint : blueprintService.getAll()) {
                 var rawData = dataFetcher.fetchData(blueprint.getAddresses());
                 var processedData = dataProcessor.flattenPLCData(rawData);
                 webSocketPublisher.fireSendStationData(processedData, blueprint.getId());
