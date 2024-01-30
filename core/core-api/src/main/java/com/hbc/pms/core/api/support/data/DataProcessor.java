3b1edc76da2d1263558241c22004ace7cc811d7c
Thinh Le <letienthinh0109@gmail.com>
2024-01-30 13:52:50 +0700

    change websocket return format from addrress:value to figureId:value

--- core/core-api/src/main/java/com/hbc/pms/core/api/support/data/DataProcessor.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/support/data/DataProcessor.java
@@ -2,6 +2,7 @@ package com.hbc.pms.core.api.support.data;
 
 import com.hbc.pms.core.model.Blueprint;
 import com.hbc.pms.plc.api.IoResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.plc4x.java.api.value.PlcValue;
 import org.springframework.stereotype.Component;
 
@@ -11,29 +12,48 @@ import java.util.Map;
 import java.util.Objects;
 
 @Component
+@Slf4j
 public class DataProcessor {
-    public Map<String, String> flattenPLCData(Map<String, IoResponse> rawData) {
-        Map<String, String> flattenedData = new HashMap<>();
+  public Map<String, String> flattenPLCData(Map<String, IoResponse> rawData) {
+    Map<String, String> flattenedData = new HashMap<>();
 
-        for (Map.Entry<String, IoResponse> entry : rawData.entrySet()) {
-            PlcValue plcValue = entry.getValue().getPlcValue();
-            if (Objects.isNull(plcValue)) {
-                continue;
-            }
-            flattenedData.put(entry.getKey(), plcValue.getObject().toString());
+    for (Map.Entry<String, IoResponse> entry : rawData.entrySet()) {
+      PlcValue plcValue = entry.getValue().getPlcValue();
+      if (Objects.isNull(plcValue)) {
+        continue;
+      }
+      flattenedData.put(entry.getKey(), plcValue.getObject().toString());
+    }
+    return flattenedData;
+  }
+
+  public Map<String, String> flattenToFigureMappedData(Map<String, IoResponse> rawData, Blueprint blueprint) {
+    Map<String, String> flattenedData = new HashMap<>();
+    var addressToFigureMap = blueprint.getAddressToSensorMap();
+
+    for (Map.Entry<String, List<String>> addrToFigEntry :
+      addressToFigureMap.entrySet()) {
+      for (String figureId : addrToFigEntry.getValue()) {
+        try {
+          flattenedData.put(figureId, rawData.get(addrToFigEntry.getKey()).getPlcValue().getObject().toString());
+        } catch (NullPointerException e) {
+          log.debug("Address {} of figureID {} is not found in PLC", addrToFigEntry.getKey(), figureId);
         }
-        return flattenedData;
+      }
     }
 
-    public Map<String, Map<String, String>> process(Map<String, IoResponse> rawData, List<Blueprint> blueprintList) {
-        Map<String, Map<String, String>> result = new HashMap<>();
-        for (Blueprint blueprint : blueprintList) {
-            Map<String, IoResponse> blueprintResponse = new HashMap<>();
-            for (String address : blueprint.getAddresses()) {
-                blueprintResponse.put(address, rawData.get(address));
-            }
-            result.put(blueprint.getName(), flattenPLCData(blueprintResponse));
-        }
-        return result;
+    return flattenedData;
+  }
+
+  public Map<String, Map<String, String>> process(Map<String, IoResponse> rawData, List<Blueprint> blueprintList) {
+    Map<String, Map<String, String>> result = new HashMap<>();
+    for (Blueprint blueprint : blueprintList) {
+      Map<String, IoResponse> blueprintResponse = new HashMap<>();
+      for (String address : blueprint.getAddresses()) {
+        blueprintResponse.put(address, rawData.get(address));
+      }
+      result.put(blueprint.getName(), flattenToFigureMappedData(rawData, blueprint));
     }
+    return result;
+  }
 }
