f9819f6c300e97cb1436473d2006dd6b53bff84e
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-01 21:47:44 +0700

    Add MailSenderConfig and update EmailChannel to send email notifications

---
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/MailSenderConfig.java
@@ -0,0 +1,26 @@
+package com.hbc.pms.core.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailSenderConfig {
+  @Bean
+  public JavaMailSender javaMailSender() {
+    var mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("mail.smtp2go.com");
+    mailSender.setPort(2525);
+    mailSender.setUsername("pms@ohtgo.me");
+    mailSender.setPassword("P@ssw0rd000");
+
+    var props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
+    return mailSender;
+  }
+}
