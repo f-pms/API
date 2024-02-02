a56ae2317b4ecd5ecaac625495dddffecf3b6896
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-02 10:41:02 +0700

    Remove debug log

--- core/core-api/src/main/java/com/hbc/pms/core/api/config/MailSenderConfig.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/MailSenderConfig.java
@@ -19,7 +19,6 @@ public class MailSenderConfig {
     props.put("mail.transport.protocol", "smtp");
     props.put("mail.smtp.auth", "true");
     props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
 
     return mailSender;
   }
