36dceeee936272f1511b77dde07af665f3ef4d71
Thinh Le <letienthinh0109@gmail.com>
2024-02-04 19:41:33 +0700

    alarm conditon controller

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/AlarmConditionController.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/AlarmConditionController.java
@@ -7,20 +7,40 @@ import com.hbc.pms.core.api.support.response.ApiResponse;
 import com.hbc.pms.core.model.AlarmCondition;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("alarm-conditions")
 @RequiredArgsConstructor
 public class AlarmConditionController {
+  private final ModelMapper mapper;
   private final AlarmConditionService alarmConditionService;
+  private final AlarmConditionPersistenceService alarmConditionPersistenceService;
 
   @PostMapping()
   public ApiResponse<AlarmCondition> create(@Valid @RequestBody CreateAlarmConditionCommand body) {
     AlarmCondition result = alarmConditionService.createAlarmCondition(body);
     return ApiResponse.success(result);
+  }
+
+  @GetMapping()
+  public ApiResponse<List<AlarmCondition>> getAll() {
+    List<AlarmCondition> alarmConditions = alarmConditionPersistenceService
+            .getAll()
+            .stream()
+            .map(ac -> mapper.map(ac, AlarmCondition.class))
+            .toList();
+
+    return ApiResponse.success(alarmConditions);
+  }
+
+  @GetMapping("/{alarmConditionId}")
+  public ApiResponse<AlarmCondition> getById(@PathVariable long alarmConditionId) {
+    AlarmCondition condition = alarmConditionPersistenceService.getById(alarmConditionId);
+
+    return ApiResponse.success(condition);
   }
 }
--- core/core-api/src/main/java/com/hbc/pms/core/api/service/AlarmConditionPersistenceService.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/service/AlarmConditionPersistenceService.java
@@ -1,5 +1,7 @@
 package com.hbc.pms.core.api.service;
 
+import com.hbc.pms.core.api.support.error.CoreApiException;
+import com.hbc.pms.core.api.support.error.ErrorType;
 import com.hbc.pms.core.model.AlarmCondition;
 import com.hbc.pms.core.model.Blueprint;
 import com.hbc.pms.integration.db.entity.AlarmConditionEntity;
@@ -8,6 +10,9 @@ import com.hbc.pms.integration.db.repository.AlarmConditionRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +23,21 @@ public class AlarmConditionPersistenceService {
   public AlarmCondition create(AlarmCondition alarmCondition) {
     var entity = mapper.map(alarmCondition, AlarmConditionEntity.class);
     return mapper.map(alarmConditionRepository.save(entity), AlarmCondition.class);
+  }
+
+  public List<AlarmCondition> getAll() {
+    return StreamSupport
+            .stream(alarmConditionRepository.findAll().spliterator(), false)
+            .map(b -> mapper.map(b, AlarmCondition.class))
+            .toList();
+  }
+
+  public AlarmCondition getById(long id) {
+    var oCondition = alarmConditionRepository.findById(id);
+    if (oCondition.isEmpty()) {
+      throw new CoreApiException(ErrorType.NOT_FOUND_ERROR, "Not found Alarm Condition with id: " + id);
+    }
+
+    return mapper.map(oCondition.get(), AlarmCondition.class);
   }
 }
