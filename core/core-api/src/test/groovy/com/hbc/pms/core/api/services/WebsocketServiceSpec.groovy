7a33963884f8be28245e08ae6ed45b5b665f4398
Huy XPS <huybui479@gmail.com>
2024-02-04 20:59:35 +0700

    dirty fix

--- core/core-api/src/test/groovy/com/hbc/pms/core/api/services/WebsocketServiceSpec.groovy
+++ core/core-api/src/test/groovy/com/hbc/pms/core/api/services/WebsocketServiceSpec.groovy
@@ -1,12 +1,10 @@
 package com.hbc.pms.core.api.services
 
-
 import com.hbc.pms.core.api.test.setup.FunctionalTestSpec
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class WebsocketServiceSpec extends FunctionalTestSpec {
-
 
   def "Websocket sends correct PLC values"() {
     def target = "%DB9:13548:REAL"
@@ -14,13 +12,13 @@ class WebsocketServiceSpec extends FunctionalTestSpec {
     plcValueTestFactory.setCurrentValue(target, 5f)
 
     then: "Received event with value = 5.0"
-    assertPlcTagWithValue(target, "5.0")
+    assertPlcTagWithValue(sensorConfiguration.id, "5.0")
 
     when: "Set tag to 15f"
-    plcValueTestFactory.setCurrentValue(target, 25f)
+    plcValueTestFactory.setCurrentValue(target, 15f)
 
     then: "Received event with value = 15.0"
-    assertPlcTagWithValue(target, "15.0")
+    assertPlcTagWithValue(sensorConfiguration.id, "15.0")
   }
 
 }
--- core/core-api/src/test/groovy/com/hbc/pms/core/api/test/setup/FunctionalTestSpec.groovy
+++ core/core-api/src/test/groovy/com/hbc/pms/core/api/test/setup/FunctionalTestSpec.groovy
@@ -1,22 +1,44 @@
 package com.hbc.pms.core.api.test.setup
 
 import com.hbc.pms.core.api.CoreApiApplication
+import com.hbc.pms.core.api.service.BlueprintPersistenceService
+import com.hbc.pms.core.api.service.SensorConfigurationPersistenceService
+import com.hbc.pms.core.model.Blueprint
+import com.hbc.pms.core.model.SensorConfiguration
+import com.hbc.pms.plc.api.PlcConnector
 import com.hbc.pms.support.spock.test.AbstractFunctionalSpec
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import spock.util.concurrent.PollingConditions
 
 @SpringBootTest(classes = [CoreApiApplication])
 @ActiveProfiles("test")
 class FunctionalTestSpec extends AbstractFunctionalSpec {
-  PollingConditions conditions = new PollingConditions(timeout: 10)
+  PollingConditions conditions = new PollingConditions(timeout: 20)
+  @Autowired
+  PlcConnector connector
 
-  void assertPlcTagWithValue(String address, String value) {
+  @Autowired
+  BlueprintPersistenceService blueprintPersistenceService
+  @Autowired SensorConfigurationPersistenceService configurationPersistenceService
+
+  SensorConfiguration sensorConfiguration
+  def setup() {
+    Blueprint blueprint = Blueprint.builder().name("Test")
+            .description("desc")
+            .build()
+    var createdBlueprint = blueprintPersistenceService.create(blueprint)
+    configurationPersistenceService.create(createdBlueprint.id, new SensorConfiguration(address: "%DB9:13548:REAL", x: 0, y: 0))
+    sensorConfiguration = blueprintPersistenceService.getById(1).sensorConfigurations[0]
+    connector.updateScheduler()
+  }
+
+  void assertPlcTagWithValue(Long id, String value) {
     conditions.eventually {
       eventCollector.getEvent { r ->
         {
-          r.payload.get(address) == value
+          r.payload.get(id.toString()) == value
         }
       }
     }
