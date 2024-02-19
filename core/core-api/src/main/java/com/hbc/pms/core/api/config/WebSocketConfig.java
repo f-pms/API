0cf075714666ad8bb627dd3cd95f8ed2c2f1830c
oHTGo <nguyennhathuy.orit@gmail.com>
2024-02-19 12:15:12 +0700

    Remove websocket interceptor

--- core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.hbc.pms.core.api.config;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,14 +12,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  private final WebSocketMetricInterceptor webSocketMetricLogger;
-
   @Value("#{'${hbc.origins}'.split(',')}")
   private List<String> allowedOrigins;
-
-  public WebSocketConfig(WebSocketMetricInterceptor webSocketMetricLogger) {
-    this.webSocketMetricLogger = webSocketMetricLogger;
-  }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -32,8 +25,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     config.enableSimpleBroker("/topic");
   }
 
-  @Override
-  public void configureClientInboundChannel(ChannelRegistration registration) {
-    registration.interceptors(webSocketMetricLogger);
-  }
 }
