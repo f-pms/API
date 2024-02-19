7b44ab720a816104168aa85f18ecdf3e94118610
Nguyễn Nhật Huy <nguyennhathuy.orit@gmail.com>
2024-02-19 13:26:03 +0700

    refactor: Enhanced alarm (#37)

--- core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.hbc.pms.core.api.config;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,14 +12,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  private final WebSocketMetricInterceptor webSocketMetricLogger;
-
   @Value("#{'${hbc.origins}'.split(',')}")
   private List<String> allowedOrigins;
-
-  public WebSocketConfig(WebSocketMetricInterceptor webSocketMetricLogger) {
-    this.webSocketMetricLogger = webSocketMetricLogger;
-  }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -30,10 +23,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker("/topic");
-  }
-
-  @Override
-  public void configureClientInboundChannel(ChannelRegistration registration) {
-    registration.interceptors(webSocketMetricLogger);
   }
 }
--- core/core-api/src/main/java/com/hbc/pms/core/api/support/notification/Channel.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/support/notification/Channel.java
@@ -2,8 +2,33 @@ package com.hbc.pms.core.api.support.notification;
 
 import com.hbc.pms.core.model.AlarmAction;
 import com.hbc.pms.core.model.AlarmCondition;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 
-public interface Channel {
+@Slf4j
+public abstract class Channel {
 
-  void notify(AlarmAction action, AlarmCondition condition);
+  @Retryable(backoff = @Backoff(delay = 1000))
+  public void notify(AlarmAction action, AlarmCondition condition) {
+    if (!filter(action)) {
+      return;
+    }
+
+    send(action, condition);
+  }
+
+  @Recover
+  void recover(Exception ex, AlarmAction action, AlarmCondition condition) {
+    log.error(
+        "Failed to send notification for action={}, condition={} with exception={}",
+        action.getId(),
+        condition.getId(),
+        ex.getMessage());
+  }
+
+  protected abstract boolean filter(AlarmAction action);
+
+  protected abstract void send(AlarmAction action, AlarmCondition condition);
 }
