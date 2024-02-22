23cba9234e9d3864ec4f28a5c81a600a33f50e8b
thinhle <letienthinh0109@gmail.com>
2024-02-23 01:26:16 +0700

    swagge

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/response/AlarmHistoryResponse.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/response/AlarmHistoryResponse.java
@@ -1,7 +1,5 @@
 package com.hbc.pms.core.api.controller.v1.response;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.hbc.pms.core.model.AlarmCondition;
 import com.hbc.pms.core.model.enums.AlarmStatus;
 import java.time.OffsetDateTime;
@@ -14,10 +12,8 @@ public class AlarmHistoryResponse {
   private Long id;
   private AlarmStatus status;
 
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
   private OffsetDateTime createdAt;
 
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
   private OffsetDateTime updatedAt;
   private AlarmCondition alarmCondition;
 }
