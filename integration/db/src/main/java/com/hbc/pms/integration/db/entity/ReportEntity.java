7621ea59ca2a132f465d8918abf6e6035596a3f0
oHTGo <nguyennhathuy.orit@gmail.com>
2024-03-14 00:24:49 +0700

    Add entities

---
+++ integration/db/src/main/java/com/hbc/pms/integration/db/entity/ReportEntity.java
@@ -0,0 +1,45 @@
+package com.hbc.pms.integration.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "report")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class ReportEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private OffsetDateTime recordingDate;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(nullable = false)
+  private ReportTypeEntity type;
+
+  @OneToMany(mappedBy = "report", fetch = FetchType.LAZY)
+  private List<ReportRowEntity> rows;
+}
