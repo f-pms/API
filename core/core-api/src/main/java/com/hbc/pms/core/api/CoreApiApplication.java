486c8ee2c7b598473983e69daf49dfb407b1da47
oHTGo <nguyennhathuy.orit@gmail.com>
2024-03-14 01:18:15 +0700

    Clean up

--- core/core-api/src/main/java/com/hbc/pms/core/api/CoreApiApplication.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/CoreApiApplication.java
@@ -3,11 +3,9 @@ package com.hbc.pms.core.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = "com.hbc.pms.*")
-@EnableScheduling
 public class CoreApiApplication {
 
   public static void main(String[] args) {
