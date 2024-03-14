0be7ef4e9e6dd6f8f2c3a2b28ee73f794fe7f291
thinhle <letienthinh0109@gmail.com>
2024-03-14 23:14:36 +0700

    Google java format

--- core/core-api/src/main/java/com/hbc/pms/core/api/config/ModelMapperConfig.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/ModelMapperConfig.java
@@ -41,7 +41,6 @@ public class ModelMapperConfig {
     return modelMapper;
   }
 
-
   private void addCreateAlarmConditionCommandToAlarmConditionTypeMap() {
     modelMapper
         .createTypeMap(CreateAlarmConditionCommand.class, AlarmCondition.class)
