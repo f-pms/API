2a60da64efc5d60a565aa16773eec856d34b15c8
thinhle <letienthinh0109@gmail.com>
2024-03-15 13:48:19 +0700

    alarm triggering test cases

--- core/core-api/src/test/groovy/com/hbc/pms/core/api/services/AlarmFunctionalSpec.groovy
+++ core/core-api/src/test/groovy/com/hbc/pms/core/api/services/AlarmFunctionalSpec.groovy
@@ -2,6 +2,7 @@ package com.hbc.pms.core.api.services
 
 import com.hbc.pms.core.api.TestDataFixture
 import com.hbc.pms.core.api.test.setup.FunctionalTestSpec
+import com.hbc.pms.core.model.enums.AlarmSeverity
 import com.hbc.pms.core.model.enums.AlarmType
 import com.hbc.pms.core.model.enums.BlueprintType
 import com.hbc.pms.integration.db.entity.AlarmConditionEntity
@@ -11,9 +12,9 @@ import com.hbc.pms.integration.db.repository.AlarmConditionRepository
 import com.hbc.pms.integration.db.repository.AlarmHistoryRepository
 import com.hbc.pms.integration.db.repository.BlueprintRepository
 import com.hbc.pms.integration.db.repository.SensorConfigurationRepository
+import org.junit.Ignore
 import org.spockframework.spring.EnableSharedInjection
 import org.springframework.beans.factory.annotation.Autowired
-import spock.lang.PendingFeature
 import spock.lang.Shared
 import spock.util.concurrent.PollingConditions
 
@@ -33,8 +34,8 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
   AlarmHistoryRepository historyRepository
 
   static final def ONE_SECOND_CRON = "*/1 * * * * *"
-  static final def DELAY_SEC_PREDEFINED_ALARM = 2
-  static final def DELAY_SEC_CUSTOM_ALARM = 15
+  static final def DELAY_SEC_PREDEFINED_ALARM = 3
+  static final def DELAY_SEC_CUSTOM_ALARM = 8
 
   def setup() {
     conditions = new PollingConditions(timeout: DELAY_SEC_PREDEFINED_ALARM)
@@ -45,14 +46,17 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
     blueprintRepository.save(createCustomAlarmBlueprint())
   }
 
+  @Ignore
+  //TODO get more input from HuyN
   def "Alarm Websocket - PREDEFINED Alarm with 1s checkInterval and 1s timeDelay not met - Not triggered"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_BOOL_01
-    createTestAlarm(AlarmType.PREDEFINED, target, null, null)
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.PREDEFINED, target, null, null)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
-    plcValueTestFactory.setCurrentValue(target, false)
+    plcValueTestFactory.setCurrentValue(target, true)
 
     then:
     Thread.sleep(DELAY_SEC_PREDEFINED_ALARM * 1000)
@@ -60,26 +64,29 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
     historyCountBefore == historyCountAfter
   }
 
+  @Ignore
+  //TODO get more input from HuyN
   def "Alarm Websocket - PREDEFINED Alarm with 1s checkInterval and 1s timeDelay met - Triggered correctly"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_BOOL_01
-    createTestAlarm(AlarmType.CUSTOM, target, null, null)
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.PREDEFINED, target, null, null)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
     plcValueTestFactory.setCurrentValue(target, true)
 
     then:
-    conditions.within(DELAY_SEC_CUSTOM_ALARM, {
-      def historyCountAfter = historyRepository.findAll().size()
-      historyCountAfter == historyCountBefore + 1
-    })
+    Thread.sleep(DELAY_SEC_PREDEFINED_ALARM * 1000)
+    def historyCountAfter = historyRepository.findAll().size()
+    historyCountAfter == historyCountBefore + 1
   }
 
   def "Alarm Websocket - CUSTOM Alarm with min and max range and not met - Not triggered"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, 10, 20)
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.CUSTOM, target, 10, 20)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
@@ -97,7 +104,8 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
   def "Alarm Websocket - CUSTOM Alarm with min and max range and met - Triggered correctly"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, 10, 20)
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.CUSTOM, target, 10, 20)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
@@ -116,35 +124,27 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
 //  @PendingFeature
   def "Alarm Websocket - CUSTOM Alarm with min only and not met - Not triggered"() {
     given:
-    Thread.sleep(DELAY_SEC_CUSTOM_ALARM * 1000)
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, null, null)
-    def historyCountBefore
-    def historyCountAfter
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.CUSTOM, target, 10, null)
+    def historyCountBefore = historyRepository.findAll().size()
 
     when:
-    historyCountBefore = historyRepository.findAll().size()
-    plcValueTestFactory.setCurrentValue(target, 10f)
-    Thread.sleep(DELAY_SEC_CUSTOM_ALARM * 1000)
-    historyCountAfter = historyRepository.findAll().size()
+    plcValueTestFactory.setCurrentValue(target, value)
 
     then:
-    historyCountAfter == historyCountBefore
-
-    when:
-    historyCountBefore = historyRepository.findAll().size()
-    plcValueTestFactory.setCurrentValue(target, 15f)
     Thread.sleep(DELAY_SEC_CUSTOM_ALARM * 1000)
-    historyCountAfter = historyRepository.findAll().size()
-
-    then:
+    def historyCountAfter = historyRepository.findAll().size()
     historyCountAfter == historyCountBefore
+
+    where:
+    value << [10f, 15f, 20f]
   }
 
   def "Alarm Websocket - CUSTOM Alarm with min only range and met - Triggered correctly"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, 10, null)
+    populateTestAlarm(AlarmType.CUSTOM, target, 10, null)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
@@ -157,11 +157,11 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
     })
   }
 
-  @PendingFeature
   def "Alarm Websocket - CUSTOM Alarm with max only and not met - Not triggered"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, null, 10)
+    conditionRepository.deleteAll()
+    populateTestAlarm(AlarmType.CUSTOM, target, null, 10)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
@@ -179,11 +179,11 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
   def "Alarm Websocket - CUSTOM Alarm with max only range and met - Triggered correctly"() {
     given:
     def target = TestDataFixture.PLC_ADDRESS_REAL_01
-    createTestAlarm(AlarmType.CUSTOM, target, null, 20)
+    populateTestAlarm(AlarmType.CUSTOM, target, null, 10)
     def historyCountBefore = historyRepository.findAll().size()
 
     when:
-    plcValueTestFactory.setCurrentValue(target, 25f)
+    plcValueTestFactory.setCurrentValue(target, 15f)
 
     then:
     conditions.within(DELAY_SEC_CUSTOM_ALARM, {
@@ -208,7 +208,7 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
             .build()
   }
 
-  void createTestAlarm(AlarmType alarmType, String address, Double min, Double max) {
+  void populateTestAlarm(AlarmType alarmType, String address, Double min, Double max) {
     def target = address
     def blueprint
             = blueprintRepository
@@ -222,6 +222,7 @@ class AlarmFunctionalSpec extends FunctionalTestSpec {
     def condition = AlarmConditionEntity.builder()
             .cron(ONE_SECOND_CRON)
             .isEnabled(true)
+            .severity(AlarmSeverity.HIGH)
             .type(alarmType)
             .min(min)
             .max(max)
