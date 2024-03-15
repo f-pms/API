805140c543ea6728473ca6c70acfc6aaa2f2622c
oHTGo <nguyennhathuy.orit@gmail.com>
2024-03-15 07:14:24 +0700

    Remove code

--- integration/db/src/main/java/com/hbc/pms/integration/db/repository/ReportTypeRepository.java
+++ integration/db/src/main/java/com/hbc/pms/integration/db/repository/ReportTypeRepository.java
@@ -4,8 +4,4 @@ import com.hbc.pms.integration.db.entity.ReportTypeEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ReportTypeRepository extends CrudRepository<ReportTypeEntity, Long> {
-
-  @Query("SELECT t FROM ReportTypeEntity t JOIN FETCH t.reports")
-  Iterable<ReportTypeEntity> findAllWithReports();
-}
+public interface ReportTypeRepository extends CrudRepository<ReportTypeEntity, Long> {}
