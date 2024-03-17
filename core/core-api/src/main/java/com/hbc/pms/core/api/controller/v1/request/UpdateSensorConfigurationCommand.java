cf54e50da44b062b0f973c9a82117d8336666722
thinhle <letienthinh0109@gmail.com>
2024-03-17 16:08:21 +0700

    fix blueprint mapping error

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/request/UpdateSensorConfigurationCommand.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/request/UpdateSensorConfigurationCommand.java
@@ -1,11 +1,13 @@
 package com.hbc.pms.core.api.controller.v1.request;
 
 import java.text.MessageFormat;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class UpdateSensorConfigurationCommand {
 
   private String address;
