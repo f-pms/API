82d43d1d740a50df138c6413429e402f0a8c7b51
oHTGo <nguyennhathuy.orit@gmail.com>
2024-03-19 00:33:10 +0700

    Add controller

---
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/ReportController.java
@@ -0,0 +1,38 @@
+package com.hbc.pms.core.api.controller.v1;
+
+import com.hbc.pms.core.api.controller.v1.common.Page;
+import com.hbc.pms.core.api.service.ReportPersistenceService;
+import com.hbc.pms.core.api.service.ReportTypePersistenceService;
+import com.hbc.pms.core.api.support.response.ApiResponse;
+import com.hbc.pms.core.model.Report;
+import com.hbc.pms.core.model.ReportType;
+import com.hbc.pms.core.model.criteria.ReportCriteria;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("reports")
+@RequiredArgsConstructor
+public class ReportController {
+  private final ReportTypePersistenceService reportTypePersistenceService;
+  private final ReportPersistenceService reportPersistenceService;
+
+  @GetMapping("types")
+  public ApiResponse<List<ReportType>> getTypes() {
+    return ApiResponse.success(reportTypePersistenceService.getAll());
+  }
+
+  @GetMapping("details")
+  public ApiResponse<Page<List<Report>>> getDetails(
+      @RequestParam(value = "page", defaultValue = "0") int page,
+      @RequestParam(value = "size", defaultValue = "10") int size,
+      ReportCriteria filter) {
+    return ApiResponse.success(
+        reportPersistenceService.getAll(filter, PageRequest.of(page + 1, size)));
+  }
+}
