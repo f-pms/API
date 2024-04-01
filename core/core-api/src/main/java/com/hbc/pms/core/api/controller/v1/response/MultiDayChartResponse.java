25e25872c1bab483b34a450dda98db978b2009f2
thinhle <letienthinh0109@gmail.com>
2024-04-01 14:16:11 +0700

    Missing Dates in chart Response

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/response/MultiDayChartResponse.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/response/MultiDayChartResponse.java
@@ -1,5 +1,6 @@
 package com.hbc.pms.core.api.controller.v1.response;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
@@ -11,6 +12,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MultiDayChartResponse {
   private List<String> labelSteps;
+
+  private Map<String, List<OffsetDateTime>> missingDates;
 
   private Map<String, Map<String, List<Double>>> data;
 }
