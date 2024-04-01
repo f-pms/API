c8b819cdac30005cba1279965bd4765568bdf959
Huy XPS <huybui479@gmail.com>
2024-04-01 14:38:02 +0700

    update command

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/request/auth/UpdateUserCommand.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/request/auth/UpdateUserCommand.java
@@ -1,13 +1,21 @@
 package com.hbc.pms.core.api.controller.v1.request.auth;
 
+import com.hbc.pms.core.api.constaint.RegexConstraints;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UpdateUserCommand {
   @Length(min = 3, max = 30)
+  private String oldPassword;
+
+  @Length(min = 3, max = 30)
   private String password;
 
   @Length(min = 3, max = 50)
   private String fullName;
+
+  @Email(regexp = RegexConstraints.MAIL_EXPRESSION)
+  private String email;
 }
