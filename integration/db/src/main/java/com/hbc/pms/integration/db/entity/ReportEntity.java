5ee9afd80691d2b526a6865de65ab27a9627e496
Thinh Le <letienthinh0109@gmail.com>
2024-04-05 11:14:54 +0700

    Fix lint

--- integration/db/src/main/java/com/hbc/pms/integration/db/entity/ReportEntity.java
+++ integration/db/src/main/java/com/hbc/pms/integration/db/entity/ReportEntity.java
@@ -18,7 +18,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "report")
