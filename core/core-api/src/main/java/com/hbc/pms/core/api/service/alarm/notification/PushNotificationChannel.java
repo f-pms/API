9998cec6e16460f5e033acf7343d366efbede947
Nguyễn Nhật Huy <nguyennhathuy.orit@gmail.com>
2024-04-05 19:50:56 +0700

    fix: Fixed body for push notification (#85)

--- core/core-api/src/main/java/com/hbc/pms/core/api/service/alarm/notification/PushNotificationChannel.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/service/alarm/notification/PushNotificationChannel.java
@@ -5,17 +5,21 @@ import static io.vavr.API.Case;
 import static io.vavr.API.Match;
 import static io.vavr.Predicates.is;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hbc.pms.core.model.AlarmAction;
 import com.hbc.pms.core.model.AlarmCondition;
 import com.hbc.pms.core.model.AlarmHistory;
 import com.hbc.pms.core.model.enums.AlarmActionType;
 import com.hbc.pms.core.model.enums.AlarmSeverity;
+import io.vavr.control.Try;
+import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -42,19 +46,27 @@ public class PushNotificationChannel extends AbstractChannel {
 
   @Override
   protected void send(AlarmHistory history, AlarmCondition condition, AlarmAction action) {
-    var header = new HttpHeaders();
-    header.add("Authorization", "Basic " + auth);
-    header.add("Title", title);
-    header.add(
-        "Priority",
+    var headers = new HttpHeaders();
+    headers.add("Authorization", "Basic " + auth);
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    var body = new HashMap<String, Object>();
+    body.put("title", title);
+    body.put("topic", "alarm");
+    body.put(
+        "priority",
         Match(condition.getSeverity())
             .of(
-                Case($(is(AlarmSeverity.URGENT)), "5"),
-                Case($(is(AlarmSeverity.HIGH)), "4"),
-                Case($(), "3")));
+                Case($(is(AlarmSeverity.URGENT)), 5),
+                Case($(is(AlarmSeverity.HIGH)), 4),
+                Case($(), 3)));
+    body.put(
+        "message",
+        "Message: " + action.getMessage() + "; TriggeredAt: " + history.getTriggeredAt());
 
-    var body = "Message: " + action.getMessage() + "; TriggeredAt: " + history.getTriggeredAt();
-    var request = new HttpEntity<>(body, header);
-    restTemplate.exchange(url + "/alarm", HttpMethod.POST, request, String.class);
+    var mapper = new ObjectMapper();
+    var request =
+        new HttpEntity<>(Try.of(() -> mapper.writeValueAsString(body)).getOrElse(""), headers);
+    restTemplate.exchange(url, HttpMethod.POST, request, String.class);
   }
 }
