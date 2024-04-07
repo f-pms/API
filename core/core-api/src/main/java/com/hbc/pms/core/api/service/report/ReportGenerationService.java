c3cef552fa49e7c5aac14c530037bbdf2a13233f
huybui38 <52276566+huybui38@users.noreply.github.com>
2024-04-07 15:07:36 +0700

    Update core/core-api/src/main/java/com/hbc/pms/core/api/service/report/ReportGenerationService.java
    Co-authored-by: Nguyễn Nhật Huy <nguyennhathuy.orit@gmail.com>

--- core/core-api/src/main/java/com/hbc/pms/core/api/service/report/ReportGenerationService.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/service/report/ReportGenerationService.java
@@ -19,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class ReportGenerationService {
   private final ReportPersistenceService reportPersistenceService;
