cadcfe11c0c6cd2bb3ff8d0ee8c95a39bca8b828
huybu <huybu@netcompany.com>
2024-04-23 16:46:55 +0700

    Init root user

---
+++ core/core-api/src/main/java/com/hbc/pms/core/api/config/auth/RootUserInitializer.java
@@ -0,0 +1,33 @@
+package com.hbc.pms.core.api.config.auth;
+
+import com.hbc.pms.core.api.service.auth.UserPersistenceService;
+import com.hbc.pms.core.model.User;
+import com.hbc.pms.core.model.enums.Role;
+import com.hbc.pms.integration.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RootUserInitializer implements ApplicationRunner {
+  private final UserRepository userRepository;
+  private final UserPersistenceService userPersistenceService;
+  private final RootUserProperties rootUserProperties;
+
+  @Override
+  public void run(ApplicationArguments args) {
+    if (userRepository.countAll() == 0) {
+      User userToCreate =
+          User.builder()
+              .email("admin@gmail.com")
+              .role(Role.ADMIN)
+              .fullName("Administrator")
+              .username(rootUserProperties.getUsername())
+              .password(rootUserProperties.getPassword())
+              .build();
+      userPersistenceService.create(userToCreate);
+    }
+  }
+}
--- integration/db/src/main/java/com/hbc/pms/integration/db/repository/UserRepository.java
+++ integration/db/src/main/java/com/hbc/pms/integration/db/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository
   Optional<UserEntity> findByUsername(String username);
 
   Optional<UserEntity> findByEmail(String email);
+  int countAll();
 }
