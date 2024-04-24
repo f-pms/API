0fb9698b609ef6833f798e7fdcf67faabd4bc28c
LeXuanDai0307 <lxdai0307@gmail.com>
2024-04-24 17:07:19 +0700

    Adjust alarm api

--- core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/AlarmHistoryController.java
+++ core/core-api/src/main/java/com/hbc/pms/core/api/controller/v1/AlarmHistoryController.java
@@ -1,6 +1,7 @@
 package com.hbc.pms.core.api.controller.v1;
 
 import com.hbc.pms.core.api.controller.v1.response.AlarmHistoryResponse;
+import com.hbc.pms.core.api.service.alarm.AlarmHistoryService;
 import com.hbc.pms.core.api.service.alarm.AlarmPersistenceService;
 import com.hbc.pms.core.model.enums.AlarmStatus;
 import com.hbc.pms.support.web.response.ApiResponse;
@@ -16,14 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AlarmHistoryController {
   private final ModelMapper mapper;
-  private final AlarmPersistenceService alarmPersistenceService;
+  private final AlarmHistoryService alarmHistoryService;
 
   @GetMapping
   public ApiResponse<List<AlarmHistoryResponse>> getAll(AlarmStatus status) {
-    List<AlarmHistoryResponse> alarmHistories =
-        alarmPersistenceService.getAllHistoriesByStatus(status).stream()
-            .map(ah -> mapper.map(ah, AlarmHistoryResponse.class))
-            .toList();
+    List<AlarmHistoryResponse> alarmHistories = alarmHistoryService.getAllHistoriesByStatus(status);
     return ApiResponse.success(alarmHistories);
   }
 }
